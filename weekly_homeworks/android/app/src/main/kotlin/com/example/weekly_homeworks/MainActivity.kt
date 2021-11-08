fcf50d5173b608b4ed011d0e1852d5a2291beec7
DESKTOP-TLRDHQ4\Alexandru_Ionita <ionitaalexandru703@gmail.com>
2021-11-08 16:20:13 +0200

    First Homework1 commit

---
+++ weekly_homeworks/android/app/src/main/kotlin/com/example/weekly_homeworks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weekly_homeworks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ weekly_homeworks/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'WeeklyHomeworks/homework1.dart';
+
+void main() {
+  runApp(HomeWork1());
+}
