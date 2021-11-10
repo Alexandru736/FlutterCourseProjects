be81f49b167321fa84b06e9dbd980064e2d2d975
DESKTOP-TLRDHQ4\Alexandru_Ionita <ionitaalexandru703@gmail.com>
2021-11-10 11:36:27 +0200

    Homework2 commit

--- weekly_homeworks/lib/WeeklyHomeworks/Homework2/homework2.dart
+++ weekly_homeworks/lib/WeeklyHomeworks/Homework2/homework2.dart
@@ -21,7 +21,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String guessText = "I'm thinking of a number between 1 and 100";
+  final String guessText = "I'm thinking of a number between 1 and 100";
   final String turnText = "It\'s your turn to guess my number";
   final String tryText = "Try a number!";
 
